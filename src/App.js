58662d54398135480a441b279d0ced528c6023e1
Ilaria <ilariaciavarella.ic@gmail.com>
2024-06-24 11:42:52 +0200

    Add bootstrap to project and create nav bar with it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './App.css';
+import './App.scss';
 
 import Home from './pages/Home'
 import Results from './pages/Results'
--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import './NavigationBar.css'
 
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+
+import { Search } from 'react-bootstrap-icons'
 import logo from '../../assets/images/veggie_bites-logo_hor.svg'
 
 export default function NavigationBar() {
     return (
-        <nav>
-            <a href='./'>
-                <img src={logo} alt='Veggie Bites logo' />
-            </a>
-        </nav>
+        <Navbar bg='tertiary' expand='md' className='shadow-sm px-3 py-1'>
+            <Navbar.Brand href='./'>
+                <img
+                    className='d-inline-block align-top'
+                    src={logo}
+                    alt='Veggie Bites logo'
+                    height='72px'
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+                <Form inline>
+                    <Stack direction='horizontal' gap={2}>
+                        <Form.Control
+                            type='search'
+                            placeholder='&ldquo;Carrot cake&rdquo;'
+                            className='px-3 py-2'
+                        />
+                        <Button type='submit' className='d-flex align-items-center px-3 py-2' ><Search size={20} className='me-2' /> Search</Button>
+                    </Stack >
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+
+        // <nav>
+        //     <a href='./'>
+        //         <img className='nav-logo' src={logo} alt='Veggie Bites logo' />
+        //     </a>
+        // </nav>
     )
 }
