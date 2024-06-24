7d18b2e323b49ec27654658334941df6f71478e2
Ilaria <ilariaciavarella.ic@gmail.com>
2024-06-24 19:00:31 +0200

    Start inserting elements in error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import Button from 'react-bootstrap/Button';
+
+import errorImage from '../assets/images/burnt_bread-404.jpg'
+
 export default function Error() {
     return (
-        <div>Error</div>
+        <main>
+            <div>
+                <h1>Oops, something went wrong...</h1>
+                <p>Sorry, the page you are looking for doesn't exist.<br />Return to the home and try another research.</p>
+                <Button href="./"  >Go back Home</Button>
+            </div>
+            <img src={errorImage} alt='Burnt slice of bread' />
+        </main>
     )
 }
