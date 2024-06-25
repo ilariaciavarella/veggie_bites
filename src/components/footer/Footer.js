e13865948410bf65fefe37525aa545ed09922c14
Ilaria <ilariaciavarella.ic@gmail.com>
2024-06-25 19:03:16 +0200

    Complete error page and stick footer to bottom

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
 import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
 import { Instagram, Github, Linkedin } from 'react-bootstrap-icons'
 
 export default function Footer() {
     return (
         <footer
-            className='d-flex flex-wrap justify-content-evenly row-gap-3 column-gap-5 align-items-center py-4 px-2 mt-5 mx-3 border-top border-dark'>
-            <small className='text-center' >© 2024 Veggie Bites by Ilaria Ciavarella &ndash; All rights reserved</small>
-            <Stack direction='horizontal' gap={3}  >
-                <a href='https://www.linkedin.com/in/ilaria-ciavarella' className='text-dark' >
-                    <Linkedin size={24} />
-                </a>
-                <a href='https://github.com/ilariaciavarella' className='text-dark' >
-                    <Github size={24} />
-                </a>
-                <a href='https://www.instagram.com/lil.ciavarella/' className='text-dark' >
-                    <Instagram size={24} />
-                </a>
-            </Stack>
+            className='px-3 w-100 mt-auto'>
+            <Container fluid className='d-flex flex-wrap justify-content-evenly align-items-center 
+            row-gap-3 column-gap-5 py-4 border-top border-dark'>
+                <small className='text-center' >© 2024 Veggie Bites by Ilaria Ciavarella &ndash; All rights reserved</small>
+                <Stack direction='horizontal' gap={3}  >
+                    <a href='https://www.linkedin.com/in/ilaria-ciavarella' className='text-dark' >
+                        <Linkedin size={24} />
+                    </a>
+                    <a href='https://github.com/ilariaciavarella' className='text-dark' >
+                        <Github size={24} />
+                    </a>
+                    <a href='https://www.instagram.com/lil.ciavarella/' className='text-dark' >
+                        <Instagram size={24} />
+                    </a>
+                </Stack>
+            </Container>
         </footer>
     )
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import errorImage from '../assets/images/burnt_bread-404.jpg'
 
 export default function Error() {
     return (
-        <main>
-            <div>
-                <h1>Oops, something went wrong...</h1>
-                <p>Sorry, the page you are looking for doesn't exist.<br />Return to the home and try another research.</p>
-                <Button href="./"  >Go back Home</Button>
-            </div>
-            <img src={errorImage} alt='Burnt slice of bread' />
+        <main className='flex-grow-1 d-flex align-items-center'>
+            <Container fluid className='p-0 mb-5'>
+                <Row className='m-0 align-items-center'>
+                    <Col xs={12} md={6} className='py-5 px-md-5' >
+                        <div>
+                            <h1>Oops, something went wrong...</h1>
+                            <p>Sorry, the page you are looking for doesn't exist.<br />Return to the home and try another research.</p>
+                            <Button href="./"  >Go back Home</Button>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} >
+                        <img src={errorImage} alt='Burnt slice of bread' className='img-fluid w-100 rounded' />
+                    </Col>
+                </Row>
+            </Container>
         </main>
     )
 }
