c249dea14782bb010b60f06d4c2b9abd8fac4681
Ilaria <ilariaciavarella.ic@gmail.com>
2024-07-16 15:36:38 +0200

    Change Search Form to React Router Form

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,42 +1,43 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useSearchParams } from 'react-router-dom';
 
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 import { updateQuery } from '../../features/query/querySlice';
 
 export default function SearchBar(props) {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState('');
+    const [isFormInvalid, setIsFormInvalid] = useState(false);
 
     function handleChange(e) {
+        setIsFormInvalid(false)
         setQuery(e.target.value);
     }
 
     function handleQuerySubmit(e) {
         e.preventDefault();
-        dispatch(updateQuery(query.trim()));
-        localStorage.setItem('storedQuery', query.trim());
-        if (query.trim()) {
-            navigate(`/search?query=${encodeURIComponent(query)}`);
+        if (query.match(/^[a-zA-Z0-9-]+(?:\s+[a-zA-Z0-9-]+)*$/)) {
+            setSearchParams(query);
+        } else {
+            setIsFormInvalid(true)
         }
     }
 
     return (
-        <Form className='d-flex gap-2' onSubmit={handleQuerySubmit}>
+        <Form className='d-flex gap-2' role='search' id='search-form' action='/search'>
             <InputGroup>
-                <Form.Control
+                <input
+                    className={!isFormInvalid ? `form-control form-control-${props.size}` : `form-control form-control-${props.size} is-invalid`}
                     name='search'
                     type='search'
                     placeholder='&ldquo;Carrot cake&rdquo;'
-                    size={props.size}
                     value={query}
                     onChange={handleChange}
+                    required
                 />
                 <Button type='submit' className='d-flex align-items-center' size={props.size} variant={props.color}>
                     <i className="bi bi-search"></i>
