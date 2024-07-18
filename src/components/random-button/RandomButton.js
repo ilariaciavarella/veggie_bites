25b02feb6703c7e975195f4cb0bb5e72a8bc9c58
Ilaria <ilariaciavarella.ic@gmail.com>
2024-07-18 18:29:48 +0200

    Make random button work

--- src/components/random-button/RandomButton.js
+++ src/components/random-button/RandomButton.js
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import Button from 'react-bootstrap/esm/Button'
+import Button from 'react-bootstrap/esm/Button';
 
 export default function RandomButton() {
-    const [randomId, setRandomId] = useState('');
+    const navigate = useNavigate();
 
-    useEffect(() => {
+    async function handleClick(e) {
+        e.preventDefault();
         const client = axios.create({
             baseURL: process.env.REACT_APP_API_BASE_URL
         });
-
-        client.get(`random?apiKey=${process.env.REACT_APP_API_KEY}&number=1&include-tags=vegetarian`).then((response) => {
-            console.log(response.data.recipes);
-            setRandomId(response.data.recipes[0].id);
-        });
-    }, [])
-
+        await client.get(`random?apiKey=${process.env.REACT_APP_API_KEY}&number=1&include-tags=vegetarian`)
+            .then(response => response.data.recipes[0].id)
+            .then(id => {
+                navigate(`/recipes/${id}`)
+            })
+    }
 
     return (
-        <Link to={`../recipes/${randomId}`} relative='path' className='text-decoration-none'>
-            <Button
-                className='w-100 mt-2 d-flex align-items-center justify-content-center gap-3'
-                variant='outline-secondary'
-                size='lg'
-            >
-                <i className='bi bi-shuffle'></i>
-                Get a random recipe
-            </Button>
-        </Link>
+        <Button
+            className='w-100 mt-2 d-flex align-items-center justify-content-center gap-3'
+            variant='outline-secondary'
+            size='lg'
+            onClick={handleClick}
+        >
+            <i className='bi bi-shuffle'></i>
+            Get a random recipe
+        </Button>
     )
 }
