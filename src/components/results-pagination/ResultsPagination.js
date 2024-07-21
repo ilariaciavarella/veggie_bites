970a3633ec5d59accf4816fdc16e65d370e41a90
Ilaria <ilariaciavarella.ic@gmail.com>
2024-07-21 19:15:29 +0200

    Delete useless code

--- src/components/results-pagination/ResultsPagination.js
+++ src/components/results-pagination/ResultsPagination.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import ResponsivePagination from 'react-responsive-pagination';
 import { dropEllipsis, dropFirstAndLast, combine } from 'react-responsive-pagination/narrowBehaviour'
